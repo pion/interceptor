8cf0fdac004145887ebe4a59ae9db4ff94ce2fd5
Mathis Engelbart <mathis.engelbart@gmail.com>
2022-07-11 21:54:07 +0200

    Implement RTP/RTCP related stats
    This Commit adds the RTP and RTCP related statistics from the WebRTC
    statistics API (https://w3c.github.io/webrtc-stats/) draft, that are
    easy to gather by intercepting RTP/RTCP streams.

---
+++ internal/ntp/ntp_test.go
@@ -0,0 +1,49 @@
+package ntp
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNTPTimeConverstion(t *testing.T) {
+	for i, cc := range []struct {
+		ts time.Time
+	}{
+		{
+			ts: time.Now(),
+		},
+		{
+			ts: time.Unix(0, 0),
+		},
+	} {
+		t.Run(fmt.Sprintf("TimeToNTP/%v", i), func(t *testing.T) {
+			assert.InDelta(t, cc.ts.UnixNano(), ToTime(ToNTP(cc.ts)).UnixNano(), float64(time.Millisecond.Nanoseconds()))
+		})
+	}
+}
+
+func TestTimeToNTPConverstion(t *testing.T) {
+	for i, cc := range []struct {
+		ts uint64
+	}{
+		{
+			ts: 0,
+		},
+		{
+			ts: 65535,
+		},
+		{
+			ts: 16606669245815957503,
+		},
+		{
+			ts: 9487534653230284800,
+		},
+	} {
+		t.Run(fmt.Sprintf("TimeToNTP/%v", i), func(t *testing.T) {
+			assert.Equal(t, cc.ts, ToNTP(ToTime(cc.ts)))
+		})
+	}
+}
