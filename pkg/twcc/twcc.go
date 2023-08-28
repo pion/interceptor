a646d66417971ae51b36f655a7496ea7fff166ae
treyhakanson <treyhakanson@gmail.com>
2023-08-27 19:38:54 -0700

    Defer packet ordering until building RTCP packet
    The previous implementation did a sorted insert (O(m)) on
    every call to `Record`. If sorting is deferred until a
    feedback packet is built, we can record in constant time
    and build a packet in O(nlogn). Ordering isn't required
    until building the packet anyway, and deferring nets a minor
    performance gain (I say minor since its unlikely there are a
    large number of received packets in the buffer prior to
    building a feedback packet). If ordering is really needed on
    record, we could use something like a B-tree to get O(logn)
    sorted inserts.

--- pkg/twcc/twcc.go
+++ pkg/twcc/twcc.go
@@ -6,6 +6,7 @@ package twcc
 
 import (
 	"math"
+	"sort"
 
 	"github.com/pion/rtcp"
 )
@@ -44,10 +45,8 @@ func (r *Recorder) Record(mediaSSRC uint32, sequenceNumber uint16, arrivalTime i
 	if sequenceNumber < 0x0fff && (r.lastSequenceNumber&0xffff) > 0xf000 {
 		r.cycles += 1 << 16
 	}
-	r.receivedPackets = insertSorted(r.receivedPackets, pktInfo{
-		sequenceNumber: r.cycles | uint32(sequenceNumber),
-		arrivalTime:    arrivalTime,
-	})
+	pkt := pktInfo{r.cycles | uint32(sequenceNumber), arrivalTime}
+	r.receivedPackets = append(r.receivedPackets, pkt)
 	r.lastSequenceNumber = sequenceNumber
 }
 
@@ -56,45 +55,41 @@ func (r *Recorder) PacketsHeld() int {
 	return len(r.receivedPackets)
 }
 
-func insertSorted(list []pktInfo, element pktInfo) []pktInfo {
-	if len(list) == 0 {
-		return append(list, element)
-	}
-	for i := len(list) - 1; i >= 0; i-- {
-		if list[i].sequenceNumber < element.sequenceNumber {
-			list = append(list, pktInfo{})
-			copy(list[i+2:], list[i+1:])
-			list[i+1] = element
-			return list
-		}
-		if list[i].sequenceNumber == element.sequenceNumber {
-			list[i] = element
-			return list
-		}
-	}
-	// element.sequenceNumber is between 0 and first ever received sequenceNumber
-	return append([]pktInfo{element}, list...)
-}
-
 // BuildFeedbackPacket creates a new RTCP packet containing a TWCC feedback report.
 func (r *Recorder) BuildFeedbackPacket() []rtcp.Packet {
-	if len(r.receivedPackets) < 2 {
+	// sort received packets by sequence number, with earliest arrivalTime first in cases of duplicates
+	canBuild := false
+	sort.Slice(r.receivedPackets, func(i, j int) bool {
+		if r.receivedPackets[i].sequenceNumber == r.receivedPackets[j].sequenceNumber {
+			return r.receivedPackets[i].arrivalTime < r.receivedPackets[j].arrivalTime
+		}
+		canBuild = true // need at least 2 non-duplicate packets
+		return r.receivedPackets[i].sequenceNumber < r.receivedPackets[j].sequenceNumber
+	})
+	if !canBuild {
 		return nil
 	}
 
 	feedback := newFeedback(r.senderSSRC, r.mediaSSRC, r.fbPktCnt)
 	r.fbPktCnt++
-	feedback.setBase(uint16(r.receivedPackets[0].sequenceNumber&0xffff), r.receivedPackets[0].arrivalTime)
+	feedback.setBase(
+		uint16(r.receivedPackets[0].sequenceNumber&0xffff),
+		r.receivedPackets[0].arrivalTime,
+	)
 
 	var pkts []rtcp.Packet
-	for _, pkt := range r.receivedPackets {
-		ok := feedback.addReceived(uint16(pkt.sequenceNumber&0xffff), pkt.arrivalTime)
-		if !ok {
-			pkts = append(pkts, feedback.getRTCP())
-			feedback = newFeedback(r.senderSSRC, r.mediaSSRC, r.fbPktCnt)
-			r.fbPktCnt++
-			feedback.addReceived(uint16(pkt.sequenceNumber&0xffff), pkt.arrivalTime)
+	var prevSN uint32
+	for i, pkt := range r.receivedPackets {
+		if i == 0 || pkt.sequenceNumber != prevSN {
+			ok := feedback.addReceived(uint16(pkt.sequenceNumber&0xffff), pkt.arrivalTime)
+			if !ok {
+				pkts = append(pkts, feedback.getRTCP())
+				feedback = newFeedback(r.senderSSRC, r.mediaSSRC, r.fbPktCnt)
+				r.fbPktCnt++
+				feedback.addReceived(uint16(pkt.sequenceNumber&0xffff), pkt.arrivalTime)
+			}
 		}
+		prevSN = pkt.sequenceNumber
 	}
 	r.receivedPackets = []pktInfo{}
 	pkts = append(pkts, feedback.getRTCP())
