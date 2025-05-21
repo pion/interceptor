8d3fc6df95a31cac930664b251e6c8c9d1493f47
Aleksandr Alekseev <alekseev-dev@yandex-team.ru>
2025-05-21 17:38:32 +0300

    Test encoder with different payload sizes

--- pkg/flexfec/flexfec_encoder_03_test.go
+++ pkg/flexfec/flexfec_encoder_03_test.go
@@ -61,3 +61,38 @@ func TestFlexEncoder03_EncodeFec_MissingPackets(t *testing.T) {
 	result := encoder.EncodeFec(mediaPackets, 1)
 	assert.Nil(t, result, "EncodeFec should return nil when there are missing packets")
 }
+
+func TestFlexEncoder03_EncodeFec_DifferentPayloadSizes(t *testing.T) {
+	encoder := NewFlexEncoder03(96, 1234)
+
+	smallPayload := []byte{1, 2, 3}
+	largePayload := []byte{1, 2, 3, 4, 5, 6, 7, 8}
+
+	mediaPackets := []rtp.Packet{
+		{
+			Header: rtp.Header{
+				SequenceNumber: 1,
+				SSRC:           1234,
+			},
+			Payload: smallPayload,
+		},
+		{
+			Header: rtp.Header{
+				SequenceNumber: 2,
+				SSRC:           1234,
+			},
+			Payload: largePayload,
+		},
+	}
+
+	fecPackets := encoder.EncodeFec(mediaPackets, 1)
+
+	assert.NotNil(t, fecPackets, "EncodeFec should return FEC packets")
+	assert.Equal(t, 1, len(fecPackets), "EncodeFec should return 1 FEC packet")
+
+	expectedPayloadSize := len(largePayload)
+	actualPayloadSize := len(fecPackets[0].Payload) - BaseFec03HeaderSize
+
+	assert.Equal(t, expectedPayloadSize, actualPayloadSize,
+		"FEC payload size should match the size of the largest media packet payload")
+}
