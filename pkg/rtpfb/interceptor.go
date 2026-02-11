cf22eff30311c39db0aa197f2f35416714ebdcd2
Pion <59523206+pionbot@users.noreply.github.com>
2026-02-11 07:22:44 +0000

    Update CI configs to v0.11.38
    Update lint scripts and CI configs.

--- pkg/rtpfb/interceptor.go
+++ pkg/rtpfb/interceptor.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2025 The Pion community <https://pion.ly>
+// SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
 // Package rtpfb implements feedback aggregation for CCFB and TWCC packets.
