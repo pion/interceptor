2b2c61732a90a01c7be25d08990a6c8600776630
Pion <59523206+pionbot@users.noreply.github.com>
2026-09-22 21:59:50 +0000

    Update CI configs to v0.12.7
    Update lint scripts and CI configs.

--- pkg/rfc8888/rfc8888.go
+++ pkg/rfc8888/rfc8888.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2025 The Pion community <https://pion.ly>
+// SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
 // Package rfc8888 is deprecated. Use ccfb instead.
